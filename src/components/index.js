23fe9aeb7b555e87c32c489bc4b6ca943ce23c5b
Carlos <>
2020-09-26 18:08:25 +0800

    feat: vue components

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+// import list1 from "./atoms/list1.vue";
+// import list2 from "./molecules/list2.vue";
+// import list3 from "./sections/list3.vue";
+// import list4 from "./templates/list4.vue";
+
+export default Object.assign({
+    // list1,
+    // 	list2,
+    // 	list3,
+    // 	list4
+});
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import "/@/styles/index.sass";
 
 import { createApp } from "vue";
+import components from "/@/components";
 import App from "./App.vue";
-import "./index.css";
 
 const Vue = createApp(App);
+
+Object.keys(components).forEach((key) => {
+    Vue.component(key, components[key]);
+});
 Vue.mount("#app");
