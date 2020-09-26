84435dafa9142a42d6b6536d877472c5e3ce3658
Carlos <>
2020-09-26 17:16:58 +0800

    chore: devtools

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./index.css";
 
-createApp(App).mount('#app')
+const Vue = createApp(App);
+Vue.mount("#app");
