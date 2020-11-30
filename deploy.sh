dc69f2d3a1e44f101a0b5e1af67edc3ed15aa6d4
Carlos <>
2020-11-30 17:40:43 +0800

    docs: deploy 備註

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
+# 打包
 vite build
+
+# 新增至暫存分支 (強制加入dist資料夾)
 git checkout -b temp
 git add -f dist
 git commit --allow-empty -m 'Initial dist subtree commit'
+
+# 分割 dist 資料夾至 gh-pages 分支
 git subtree split --rejoin --prefix dist -b gh-pages
 git push -f origin gh-pages
+
+# 切回主分支，刪除本地暫存分支
 git checkout master
 git branch -D gh-pages temp
