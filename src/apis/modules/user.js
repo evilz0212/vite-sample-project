a32c8d92207aed263660c36b818fe24eea12d745
Carlos <>
2021-03-08 10:26:03 +0800

    fix: postman mockserver update

--- src/apis/modules/user.js
+++ src/apis/modules/user.js
@@ -2,8 +2,4 @@ import req from "/@/apis/https.js"
 
 // 定義接口
 export const postLogin = (params) =>
-	req(
-		"post",
-		`https://5b84ebcd-cd9d-477f-ae29-cec2dffa0edf.mock.pstmn.io/auth`,
-		params
-	)
+	req("post", `https://29d35f3e-c32e-4ba6-b199-8a876339d448.mock.pstmn.io/auth`, params)
